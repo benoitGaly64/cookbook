10bc1a93bcb435fa439736babb45b27fd7e90d00
benoitGaly64 <34302380+benoitGaly64@users.noreply.github.com>
2021-01-26 15:05:59 +0100

    update composer

--- public/js/app.js
+++ public/js/app.js
@@ -80833,8 +80833,8 @@ if (document.getElementById('example')) {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\wamp64\www\cookbook\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\wamp64\www\cookbook\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\Dev\Perso\cookbook\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\Dev\Perso\cookbook\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
